fcc32b71b0ff8860b33e1c5cfe949e7b13daffcb
Swathi Shree S <swathishree.s@payoda.net>
2024-08-23 16:03:52 +0530

    23/08/2024Assignment

---
+++ SampleXml/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace SampleXml
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlReader xmlread = XmlReader.Create(@"C:\Users\swathishree.s\source\repos\XmlProject\XmlProject\bin\Debug\net8.0\Courses.xml");
+            while (xmlread.Read())
+            {
+                switch (xmlread.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        listBox1.Items.Add("<" + xmlread.Name + ">");
+                        break;
+                    case XmlNodeType.Text:
+                        listBox1.Items.Add(xmlread.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        listBox1.Items.Add("</Course>");
+                        break;
+                }
+            }
+
+        }
+    }
+}
