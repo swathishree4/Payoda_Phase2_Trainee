0418e0d35c1f7fa5cadf0d45c35b60e2a4b9e5fe
swathishree4 <145197235+swathishree4@users.noreply.github.com>
2024-08-29 11:35:23 +0530

    Add files via upload

---
+++ Assessment/c# Assessment/TestTask/TestTask/LifeInsurance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TestTask
+{
+    internal class LifeInsurance : Insurance
+    {
+     
+        private int policyTerm;
+        private float benefitPercent;
+
+      
+        public int PolicyTerm
+        {
+            get { return policyTerm; }
+            set { policyTerm = value; }
+        }
+
+        public float BenefitPercent
+        {
+            get { return benefitPercent; }
+            set { benefitPercent = value; }
+        }
+
+        public LifeInsurance(string insuranceNo, string insuranceName, double amountCovered, int policyTerm, float benefitPercent)
+            : base(insuranceNo, insuranceName, amountCovered)
+        {
+            PolicyTerm = policyTerm;
+            BenefitPercent = benefitPercent;
+        }
+
+        public override double CalculatePremium()
+        {
+            return (AmountCovered - ((AmountCovered * BenefitPercent) /100)) / PolicyTerm;
+        }
+    }
+}
