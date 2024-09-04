52971ce47f3780743fb3e51d722bb7cfafee6dda
swathishree4 <145197235+swathishree4@users.noreply.github.com>
2024-09-04 20:52:12 +0530

    Add files via upload

---
+++ Assessment/c# Assessment/TestTask/MVC/MVC_MAINTEST/MVC_MAINTEST/Models/ApplicationDbContext .cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_MAINTEST.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+       
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options) { }
+
+    }
+}
---
+++ Assessment/c# Assessment/TestTask/MVC/MVC_MAINTEST/MVC_MAINTEST/Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using MVC_MAINTEST.Models;
+
+namespace MVC_MAINTEST.Repository
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserByIdAsync(int id);
+        Task AddUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(int id);
+
+    }
+}
