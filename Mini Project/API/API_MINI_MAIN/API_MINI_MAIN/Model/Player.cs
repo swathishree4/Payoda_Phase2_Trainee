b25ec388d673d2ffc3e6db95d63bd5473a604f91
swathishree4 <145197235+swathishree4@users.noreply.github.com>
2024-09-11 13:59:15 +0530

    Add files via upload

---
+++ Mini Project/API/API_MINI_MAIN/API_MINI_MAIN/Model/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Apiproject.Model
+{
+    public class Player
+    {
+        [Key]
+        public int PlayerId { get; set; }
+        public string? Name { get; set; }
+        public string? Position { get; set; }
+        public int TotalPoints { get; set; }
+        public int TotalAssists { get; set; }
+        public int TotalRebounds { get; set; }
+        public ICollection<Game>? Games { get; set; }
+    }
+}
---
+++ Mini Project/API/API_MINI_MAIN/API_MINI_MAIN/Service/GameService.cs
@@ -0,0 +1,53 @@
+﻿using Apiproject.Interface;
+using Apiproject.Model;
+
+namespace Apiproject.Service
+{
+    public class GameService
+    {
+        private readonly IGame _gmrepo;
+        public GameService(IGame gmrepo)
+        {
+            _gmrepo = gmrepo;
+        }
+        public async Task<IEnumerable<Game>> GetallGm()
+        {
+            return await _gmrepo.GetGame();
+        }
+
+        public async Task<Game> Getgmbyid(int id)
+        {
+            return await _gmrepo.GetGameById(id);
+        }
+
+        public async Task postgm(Game e)
+        {
+            await _gmrepo.PostGame(e);
+        }
+
+        public async Task putgm(int id, Game e)
+        {
+            await _gmrepo.PutGame(id, e);
+        }
+
+        public async Task Deletegm(int id)
+        {
+            await _gmrepo.DeleteGame(id);
+        }
+
+        public async Task<IEnumerable<Game>> GetGamesByAttendance(int minAttendance, int maxAttendance)
+        {
+            return await _gmrepo.GetGamesByAttendance(minAttendance, maxAttendance);
+        }
+
+        public async Task<int> CountGamesByAttendance(int minAttendance, int maxAttendance)
+        {
+            return await _gmrepo.CountGamesByAttendance(minAttendance, maxAttendance);
+        }
+
+        public async Task<IEnumerable<Player>> GetPlayersByPerformance(int minPoints, int minAssists, int minRebounds)
+        {
+            return await _gmrepo.GetPlayersByPerformance(minPoints, minAssists, minRebounds);
+        }
+    }
+}
