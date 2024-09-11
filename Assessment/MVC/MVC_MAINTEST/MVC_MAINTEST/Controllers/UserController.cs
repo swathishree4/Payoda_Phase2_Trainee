191f4938e3b79ac7e94b7bb4d4a8842a290cdcaa
swathishree4 <145197235+swathishree4@users.noreply.github.com>
2024-09-11 14:00:46 +0530

    Add files via upload

---
+++ Assessment/MVC/MVC_MAINTEST/MVC_MAINTEST/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_MAINTEST.Models;
+using MVC_MAINTEST.Repository;
+
+namespace MVC_MAINTEST.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        // GET: Users
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userRepository.GetAllUsersAsync();
+            return View(users);
+        }
+
+        // GET: Users/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var user = await _userRepository.GetUserByIdAsync(id);
+            if (user == null) return NotFound();
+
+            return View(user);
+        }
+
+        // GET: Users/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Username,Email")] User user)
+        {
+            
+                await _userRepository.AddUserAsync(user);
+                return RedirectToAction(nameof(Index));
+            
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var user = await _userRepository.GetUserByIdAsync(id);
+            if (user == null) return NotFound();
+
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("uId,Username,Email")] User user)
+        {
+            if (id != user.uId) return NotFound();
+
+            
+                try
+                {
+                    await _userRepository.UpdateUserAsync(user);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (await _userRepository.GetUserByIdAsync(id) == null) return NotFound();
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            
+            return View(user);
+        }
+
+        // GET: Users/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _userRepository.GetUserByIdAsync(id);
+            if (user == null) return NotFound();
+
+            return View(user);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _userRepository.DeleteUserAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
